0b58b25383dc33bca5d39bc8e48ced1ab184da29
yazanQwaider <yazeno15@gmail.com>
2023-05-10 22:40:03 +0300

    Add videojs library and enahce design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,34 +13,35 @@ export default function Home() {
         
         videos_thumbnails.push(
             <div className={`thumbnail-video ${(currentVideoIndex === index)? 'active':''}`} key={'thumbnail-' + index}>
-                <img src={video.thumb} alt="Video thumbnail" onClick={() => changeTheCurrentVideo(index)} style={{ width: '200px' }} />
+                <img src={video.thumb} alt="Video thumbnail" onClick={() => changeTheCurrentVideo(index)} style={{ width: '150px' }} />
             </div>
         );
     }
 
     function changeTheCurrentVideo(video_index) {
-        setCurrentVideoIndex((index) => video_index);
-        setCurrentVideo(v => videos[video_index]);
+        setCurrentVideoIndex(video_index);
+        setCurrentVideo(videos[video_index]);
     }
 
     useEffect(() => {
         document.getElementsByTagName('body')[0].onkeydown = function(e) {
-            if(e.code === 'ArrowDown') {
+            if(e.code === 'ArrowDown' && currentVideoIndex < videos.length - 1) {
                 changeTheCurrentVideo(currentVideoIndex + 1);
             }
-            else if(e.code === 'ArrowUp') {
+            else if(e.code === 'ArrowUp' && currentVideoIndex > 0) {
                 changeTheCurrentVideo(currentVideoIndex - 1);
             }
         }
     });
 
     return <div>
-        <h1>Reels</h1>
-
-        <p>Welcome back, Yazan Qwaider ...</p>
-
-        <div style={{ display: "flex", justifyContent: "space-around"}}>
-            <aside>
+        <div style={{height: '20vh'}}>
+            <h1>Reels</h1>
+            <p>Welcome back, Yazan Qwaider ...</p>
+        </div>
+      
+        <div style={{ height: '80vh', display: "flex", justifyContent: "space-around"}}>
+            <aside style={{overflow: 'auto', height: '95%'}}>
                 { videos_thumbnails }
             </aside>
 
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useRef } from "react";
+import videojs from "video.js";
 
-export default function VideoComponent(props) {
+export default function VideoComponent({video}) {
     const videoTagRef = useRef();
 
     useEffect(() => {
+        videojs(videoTagRef.current);
         videoTagRef.current.load();
-    });
+    }, [video, videoTagRef]);
 
     return (
-        <video controls style={{width: '700px'}} ref={videoTagRef}>
-            <source type="video/mp4" src={props.video.source}/>
+        <video autoPlay controls style={{width: '800px', height: '400px'}} className={'video-js'} id={'video-' + video.id} ref={videoTagRef}>
+            <source type="video/mp4" src={video.source}/>
         </video>
     );
 }
